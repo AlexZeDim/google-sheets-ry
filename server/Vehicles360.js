d0bef862ec1d766320d52dc30fdfb0a04c18c5c0
Sasha <AlexZeDim@mail.ru>
2019-08-23 15:58:11 +0300

    fix shitty code

--- server/Vehicles360.js
+++ server/Vehicles360.js
@@ -1,24 +1,23 @@
-var ControlSheetName = "Аналитика по ТС";
-var DataSheetName = "Автопарк ТС";
+var AnalystSheetName = "Аналитика по ТС";
 
 function FromUnique() {
   var Array = [];
-  var Sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(ControlSheetName);
+  var Sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(AnalystSheetName);
   var Rows = Sheet.getDataRange();
   var numRows = Rows.getNumRows();
   var values = Rows.getValues();
   var Formula_QueryString = "=UNIQUE({'Журнал заказов'!D2:D})";
-  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(ControlSheetName).getRange("B10:C").clearContent();
-  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(ControlSheetName).getRange('C12').setValue(Formula_QueryString);
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(AnalystSheetName).getRange("B10:C").clearContent();
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(AnalystSheetName).getRange('C12').setValue(Formula_QueryString);
 }
 
 function FromList() {
   var Array = [];
-  var Sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(ControlSheetName);
+  var Sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(AnalystSheetName);
   var Rows = Sheet.getDataRange();
   var numRows = Rows.getNumRows();
   var values = Rows.getValues();
   var Formula_QueryString = "={'Автопарк ТС'!C2:D}";
-  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(ControlSheetName).getRange("B10:C").clearContent();
-  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(ControlSheetName).getRange('B12').setValue(Formula_QueryString);
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(AnalystSheetName).getRange("B10:C").clearContent();
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName(AnalystSheetName).getRange('B12').setValue(Formula_QueryString);
 }
